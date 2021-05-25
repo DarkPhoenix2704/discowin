ab49f8d66c83c4e36f5b1c0ed8714294f30351cd
Anbarasu <64391274+DarkPhoenix2704@users.noreply.github.com>
2021-05-25 13:46:27 +0530

    Update welcome.js

--- src/welcome.js
+++ src/welcome.js
@@ -2,8 +2,6 @@ module.exports = (client) => {
     client.on('guildMemberAdd', (member) => {
         const channel = member.guild.channels.cache.find(ch => ch.name === 'discowin')
         if (!channel) return
-        channel.send(`Welcome to the Covid Notification Server\n
-        /help for available commands\n`)
-        channel.send('Currently I\'m available only in dm. So don\'t message me here')
+        channel.send(`Hii <@${member.id}>\nWelcome to the Covid Notification Server\n/help in dm for available commands \n Currently I\m available only in dm. So dont message me here`)
     })
 }
